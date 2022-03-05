4dba7fd47a09dda2862cdafaa2a55c64d0e7a095
Kris Cox <kris.cox@cococo.be>
2022-03-05 23:13:12 +0100

    Site and Admin first entries

---
+++ com_balancirk/admin/tmpl/overview/default.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package 	com_balancirk
+ * @subpackage 	student
+ *
+ * @copyright   Copyright (C) 2022 CoCoCo. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE.txt
+ */
+
+use Joomla\CMS\Language\Text;
+
+defined('_JEXEC') or die('Restricted access');
+?>
+<h1><?php Text::_('COM_BALANCIRK_NAME') ?></h1>
+
+<h2> <?php Text::_('COM_BALANCIRK_MENU') ?></h2>
---
+++ com_balancirk/site/tmpl/student/default.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package 	com_balancirk
+ * @subpackage 	student
+ *
+ * @copyright   Copyright (C) 2022 CoCoCo. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE.txt
+ */
+
+use Joomla\CMS\Language\Text;
+
+defined('_JEXEC') or die('Restricted access');
+?>
+<h1>Students view</h1>
+
+<h2>List of students</h2>
