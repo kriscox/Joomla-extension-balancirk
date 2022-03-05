e52e49b73afcbbd2f379afa6790b734c6897539e
Kris Cox <kris.cox@cococo.be>
2022-03-05 20:41:08 +0100

    End try 1

---
+++ com_balancirk/site/src/View/Students/HtmlView.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CoCoCo\Component\Balancirk\Site\View\Students;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_balancirk
+ *
+ * @copyright   Copyright (C) 2022 CoCoCo. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * View for the user identity validation form
+ */
+class HtmlView extends BaseHtmlView
+{
+
+
+    /**
+     * Display the view
+     *
+     * @param   string  $template  The name of the layout file to parse.
+     * @return  void
+     */
+    public function display($template = null)
+    {
+        // Call the parent display to display the layout file
+        parent::display($template);
+    }
+}
