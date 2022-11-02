1b3d71065cd2f75fb06ede6f8d02fef1f4e95551
Kris Cox <kris.cox@cococo.be>
2022-11-02 17:16:58 +0100

    push

---
+++ com_balancirk/site/src/Controller/MemberController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_balancirk
+ *
+ * @copyright   Copyright (C) 2022 CoCoCo. All rights reserved.
+ * @license     GNU General Public License version 3.
+ */
+
+namespace CoCoCo\Component\Balancirk\Site\Controller;
+
+use Joomla\CMS\MVC\Controller\FormController;
+
+\defined('_JEXEC') or die;
+
+/**
+ * Balancirk member controller.
+ * 
+ * @since   0.0.1
+ */
+class MemberController extends  FormController
+{
+}
