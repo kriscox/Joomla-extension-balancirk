282bf3f9135f922521b910b3d28c4475634e7bff
Kris Cox <cox.kris@gmail.com>
2025-02-16 09:47:52 +0100

    fixed student api

--- components/com_balancirk/api/src/View/Students/JsonapiView.php
+++ components/com_balancirk/api/src/View/Students/JsonapiView.php
@@ -9,37 +9,37 @@ use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
 
 class JsonapiView extends BaseApiView
 {
-    /**
-     * The fields to render item in the documents
-     *
-     * @var     array
-     * @since  __BUMP_VERSION__
-     */
-    protected $fieldsToRenderItem = [
-        'id',
-        'name',
-        'firstname',
-        'email',
-        'street',
-        'number',
-        'bus',
-        'postcode',
-        'city',
-        'phone',
-        'birthdate',
-    ];
+        /**
+         * The fields to render item in the documents
+         *
+         * @var     array
+         * @since  __BUMP_VERSION__
+         */
+        protected $fieldsToRenderItem = [
+                'id',
+                'name',
+                'firstname',
+                'email',
+                'street',
+                'number',
+                'bus',
+                'postcode',
+                'city',
+                'phone',
+                'birthdate',
+        ];
 
-    /**
-     * The fields to renders item in the documents
-     *
-     * @var  array
-     * @since  4.0.0
-     */
-    protected $fieldsToRenderList = [
-        'id',
-        'name',
-        'firstname',
-        'email',
-        'phone'
-    ];
+        /**
+         * The fields to renders item in the documents
+         *
+         * @var  array
+         * @since  4.0.0
+         */
+        protected $fieldsToRenderList = [
+                'id',
+                'name',
+                'firstname',
+                'email',
+                'phone'
+        ];
 }
--- plugins/balancirk/balancirk.php
+++ plugins/balancirk/balancirk.php
@@ -7,17 +7,20 @@ use Joomla\CMS\Plugin\CMSPlugin;
 
 class PlgWebservicesBalancirk extends CMSPlugin
 {
-    protected $autoloadLanguage = true;
+  protected $autoloadLanguage = true;
 
-    public function onBeforeApiRoute(&$router)
-    {
-        // A nice granular way to do it.
-        // new Route(['HTTP_METHOD'],  'arbitrary/pattern/string',                     '<CONTROLLER_NAME>.<PUBLIC_METHOD_NAME>',               [], $defaults)
-        // Obviously substitute the COMPONENTNAME (lowercase no spaces), <CONTROLLER_NAME> as lowercase, & PUBLIC_METHOD_NAME as camelcase.
-        // controllers are to be placed in [site_root]/api/components/com_balancirk/src/Controllers/<CONTROLLER_NAME>Controller.php
-        $defaults   = array_merge(['public' => false], ['component' => 'com_balancirk']);
+  public function onBeforeApiRoute(&$router)
+  {
+    // A nice granular way to do it.
+    // new Route(['HTTP_METHOD'],  'arbitrary/pattern/string',                     '<CONTROLLER_NAME>.<PUBLIC_METHOD_NAME>',               [], $defaults)
+    // Obviously substitute the COMPONENTNAME (lowercase no spaces), <CONTROLLER_NAME> as lowercase, & PUBLIC_METHOD_NAME as camelcase.
+    // controllers are to be placed in [site_root]/api/components/com_balancirk/src/Controllers/<CONTROLLER_NAME>Controller.php
+    $defaults   = array_merge(['public' => false], ['component' => 'com_balancirk']);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 66fbab9 (fixed student api)
     $routes = [
       new Route(['GET'], 'v1/presence/:lesson', 'presences.getpresence', ['lesson' => '\d+'], $defaults),
       new Route(['GET'], 'v1/presence/:lesson/:date', 'presences.getpresence', ['lesson' => '\d+', 'date' => '\d{4}-\d{2}-\d{2}'], $defaults),
@@ -25,6 +28,7 @@ class PlgWebservicesBalancirk extends CMSPlugin
       new Route(['GET'], 'v1/teacher/:lesson', 'teachers.getteacher', ['lesson' => '\d+'], $defaults),
       new Route(['GET'], 'v1/teacher/:lesson/:date', 'teachers.getteacher', ['lesson' => '\d+', 'date' => '\d{4}-\d{2}-\d{2}'], $defaults),
       new Route(['POST'], 'v1/teacher/:lesson', 'teachers.setteacher', ['lesson' => '(d+)'], $defaults),
+<<<<<<< HEAD
       # Double, next lines is integrated with the createCRUDRoutes method. Check if it can be removed. The class subscriptioncontroller can afterwards also be removed.
       new Route(['DELETE'], 'v1/subscription/:id', 'subscription.delete', ['recordkey' => '\d+'], $defaults),
       new Route(['GET'], 'v1/members/me', 'members.getCurrentUser', ['recordkey' => '\d+'], $defaults),
@@ -49,12 +53,22 @@ class PlgWebservicesBalancirk extends CMSPlugin
           new Route(['POST'], 'v1/teacher/:lesson', 'teachers.setteacher', ['lesson' => '(d+)'], $defaults),
           new Route(['DELETE'], 'v1/subscription/:id', 'subscription.delete', ['recordkey' => '\d+'], $defaults)
         ];
+=======
+      new Route(['DELETE'], 'v1/subscription/:id', 'subscription.delete', ['recordkey' => '\d+'], $defaults)
+    ];
+>>>>>>> 66fbab9 (fixed student api)
 
-        // Finally, register all specified routes with Joomla's webservices router.
-        $router->addRoutes($routes);
+    // A more generic way to do it.
+    $router->createCRUDRoutes('v1/students', 'students', ['component' => 'com_balancirk']);
 
+<<<<<<< HEAD
         // A more generic way to do it.
         $router->createCRUDRoutes('v1/student', 'student', ['component' => 'com_balancirk']);
     }
 >>>>>>> 97a7dd3 (tussentijds)
+=======
+    // Finally, register all specified routes with Joomla's webservices router.
+    $router->addRoutes($routes);
+  }
+>>>>>>> 66fbab9 (fixed student api)
 }
