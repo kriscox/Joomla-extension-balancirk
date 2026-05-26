2eea813c865bcb0bab016aaf456bc27643992ebc
Kris Cox <cox.kris@gmail.com>
2026-05-26 16:45:14 +0200

    feat(spa): improve member subscriptions flow and year filtering
    - update member SPA subscriptions UI and filtering behavior
    
    - extend member API client and add lesson/presence model interfaces
    
    - sync Joomla media/build assets and package metadata

--- components/com_balancirk/admin/sql/updates/mysql/1.2.12.sql
+++ components/com_balancirk/admin/sql/updates/mysql/1.2.12.sql
@@ -1,17 +1,17 @@
 /**************************************************************************************************
- *                                                                                                 *
- *  SQL script to update lessons table                                                             *
- *                                                                                                 *
- *    added min_age and max_age columns                                                            *
- *                                                                                                 *
- **************************************************************************************************/
+*                                                                                                 *
+*  SQL script to update lessons table                                                             *
+*                                                                                                 *
+*    added min_age and max_age columns                                                            *
+*                                                                                                 *
+**************************************************************************************************/
 ALTER TABLE `#__balancirk_lessons`
-ADD COLUMN `min_age` int(11) DEFAULT NULL
-AFTER `max_students`,
-    ADD COLUMN `max_age` int(11) DEFAULT NULL
-AFTER `min_age`;
+ADD COLUMN `min_age` int(11) DEFAULT NULL AFTER `max_students`,
+ADD COLUMN `max_age` int(11) DEFAULT NULL AFTER `min_age`;
+
 CREATE OR REPLACE VIEW `#__balancirk_lessons_complete` AS
-SELECT a.`id`,
+SELECT
+    a.`id`,
     a.`name`,
     b.`name` AS `type`,
     a.`fee`,
@@ -26,16 +26,23 @@ SELECT a.`id`,
     a.`min_age`,
     a.`max_age`,
     (
-        SELECT COUNT(*)
-        FROM `#__balancirk_subscriptions`
-        WHERE `lesson` = a.`id`
+        SELECT
+            COUNT(*)
+        FROM
+            `#__balancirk_subscriptions`
+        WHERE
+            `lesson` = a.`id`
             AND `subscribed` = 0
     ) AS `numberOfStudents`,
     (
-        SELECT COUNT(*)
-        FROM `#__balancirk_subscriptions`
-        WHERE `lesson` = a.`id`
+        SELECT
+            COUNT(*)
+        FROM
+            `#__balancirk_subscriptions`
+        WHERE
+            `lesson` = a.`id`
             AND `subscribed` = 1
     ) AS `numberOnWaitingList`
-FROM `#__balancirk_lessons` a
+FROM
+    `#__balancirk_lessons` a
     INNER JOIN `#__balancirk_types` b ON a.`type` = b.`id`;
